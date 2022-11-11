cae98026a558bc60640b051e53d0df672a68826d
Rishabh Singh <rishabhjuly11@gmail.com>
2022-11-11 22:33:04 +0530

    Edit Meme

--- main/urls.py
+++ main/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
     path("",views.home, name="home"), # / route
     path("register/",views.register, name="register"), # register route
     path("login/", views.login, name="login"), # login path
     path("logout/", views.logout, name="logout"),
-    path("memes/", views.getmemes, name="memes") 
-    
-    
+    path("memes/", views.getmemes, name="memes"),
+    path("editmeme/", views.editmeme, name="edit"),
+    path("details/", views.memedetails, name = 'details')
 ]
 
--- main/views.py
+++ main/views.py
@@ -164,6 +164,7 @@ def getmemes(request):
     if sessionExists == False:
         return redirect("/login/")
     else:
+        
         # [{},{},{}]
         # Meme Name
         # Meme URL
@@ -179,4 +180,83 @@ def getmemes(request):
     
     
     
+def getmemes(request):
+    sessionExists = checkSession()
     
+    # URL:  https://api.imgflip.com/get_memes
+    
+    r = requests.get('https://api.imgflip.com/get_memes')
+    
+    meme_data = r.json()
+    
+    if sessionExists == False:
+        return redirect("/login/")
+    else:
+        
+        # [{},{},{}]
+        # Meme Name
+        # Meme URL
+        # Meme ID
+        
+        print(meme_data['data']['memes'])
+        
+        context = {
+            'memes_metadata': r.json()['data']['memes']
+        }
+        
+        return render(request,'memes.html',context=context)
+    
+    
+def editmeme(request):
+    
+    sessionStatus = checkSession()
+    
+    if sessionStatus:
+        # Display Update Page
+        template_id = request.GET['id']
+        
+        context = {
+            'meme_id' : template_id
+        }
+        
+        return render(request, 'editmeme.html', context=context)
+    else:
+        return redirect("/login/")
+
+    
+
+def memedetails(request):
+    sessionStatus = checkSession()
+    
+    if sessionStatus:
+        
+        if request.method == 'POST':
+            template_id = request.POST['id']
+            text0 = request.POST['text1']
+            text1 = request.POST['text2']
+            
+            # POST Request Meme API
+            
+            payload = {
+                'template_id' : template_id,
+                'username' : '',
+                'password' : '',
+                'text0' : text0,
+                'text1' : text1
+            }
+            
+            response = requests.request('POST','https://api.imgflip.com/caption_image',params=payload).json()
+            
+            print("Response: ")
+            print(response)
+            
+            html_str = f'''
+                         <h1>Response</h1>
+                         <img style="height: 200px; width: 200px" src="{response['data']['url']}" alt="meme">
+                         <a href="{response['data']['url']}">View Image</a>
+                       '''
+            
+            return HttpResponse(html_str)
+            
+    else:
+        return redirect("/login/")
